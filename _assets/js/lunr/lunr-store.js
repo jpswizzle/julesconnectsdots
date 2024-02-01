d42ce0b3bda757590b5797f73f4d3849d54c482a
Jules Monza <yieldovercome@Juless-MacBook-Air.local>
2024-02-01 03:55:28 -0700

    Adding fonts and Driveway 2.woah case study images

---
+++ _assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"test h1: Jules connects dots test h2: Smart design, solutions that matter h3 – Here’s a bunch of text about how awesome Jules is. You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/welcome-to-jekyll/",
+        "teaser": null
+      }]
